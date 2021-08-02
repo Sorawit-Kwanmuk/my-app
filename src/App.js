7e64200a986a99a323488a6b298236e33a467a25
SorawitKwanmuk <sorawit_kwanmuk@outlook.com>
2021-08-02 11:10:04 +0700

    Lab4.3 not success not CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='counter'>
+      <button className='btn'>+</button>
+      <span className='current'>0</span>
+      <button className='btn'>-</button>
+      <button className='btn'>Reset</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-// import { Welcome, Greeting, PI } from "./component/Welcome";
-// import * as W from "./component/Welcome";
-// import MyComponent from "./component/Welcome";
-import MyComponent, { Welcome, Greeting, PI } from "./component/Welcome";
-// function App1() {
-//   return <div>Hello React</div>;
-// }
-function App2() {
-  return (
-    <div>
-      {/* <W.Welcome />
-      <W.Greeting /> */}
-      <Welcome />
-      <Greeting />
-      <MyComponent />
-    </div>
-  );
-}
-console.log(PI);
 ReactDOM.render(
   <React.StrictMode>
-    <App2 />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
@@ -33,4 +13,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
